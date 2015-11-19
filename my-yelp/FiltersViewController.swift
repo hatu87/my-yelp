92908bddf8836cff13730bb35cdb255e0f664034
Anh-Tu Hoang <hatu87@gmail.com>
2015-11-20 00:07:16 +0700

    add filters category cell

---
+++ my-yelp/FiltersViewController.swift
@@ -0,0 +1,67 @@
+//
+//  FiltersViewController.swift
+//  my-yelp
+//
+//  Created by Anh-Tu Hoang on 11/19/15.
+//  Copyright © 2015 hatu. All rights reserved.
+//
+
+import UIKit
+
+class FiltersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var categories: [[String:String]]!
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.categories = yelpCategories()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func yelpCategories() -> [[String:String]]{
+        return [["name": "Afghan", "code":"afghani"],
+                ["name": "African", "code":"african"],
+                ["name": "American, New", "code": "newamerican"]]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onCancelButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func onSearchButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return self.categories.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath) as! SwitchTableViewCell
+        
+        cell.switchLabel.text = categories[indexPath.row]["name"]
+
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ my-yelp/SwitchTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SwitchCell.swift
+//  my-yelp
+//
+//  Created by Anh-Tu Hoang on 11/19/15.
+//  Copyright © 2015 hatu. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var switchLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
